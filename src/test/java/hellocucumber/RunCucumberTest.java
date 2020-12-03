e6b6587d815574a7151d243d6211f4feecc80113
grantsunny <grantsunny@msn.com>
2020-12-03 20:00:31 +0800

    Update some configurations to generate report

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"json:target/cucumber-report.json", "pretty"}, features="src/test/resources")
 public class RunCucumberTest {
 
 }
